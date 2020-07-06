12f94002806cd3e6f9c1f75b8c931eab91853a77
MinjuneKim <infinite.minjune.kim@gmail.com>
2020-07-06 21:27:33 +1000

    solves 33, 35 and 49

---
+++ ProblemSolving/LeetCode/49_GroupAnagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sorted_list = []
+        ret_list = dict()
+        for sample in strs:
+            _sorted = str(sorted(sample))
+            if len(sorted_list) > 0 and _sorted in sorted_list:
+                    ret_list[_sorted].append(sample)
+            else:
+                sorted_list.append(_sorted)
+                ret_list[_sorted] = [sample]
+                  
+        return [value for value in ret_list.values()]
