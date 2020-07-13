ac5b111cd6ae4ea3e6fb9b3da7911fcaa4ae5d28
MinjuneKim <infinite.minjune.kim@gmail.com>
2020-07-13 22:52:22 +1000

    solve the problem, 47

---
+++ ProblemSolving/LeetCode/47_permutations2.py
@@ -0,0 +1,22 @@
+def permutation(nums: List[int]) -> List[List[int]]:
+    data_len = len(nums)
+    if data_len == 1:
+        return [nums]
+    
+    ret_list = []
+    for idx in range(data_len):
+        tmp_list = permutation(nums[:idx] + nums[idx+1:])
+        for tmp in tmp_list:
+            sub_list = []
+            sub_list.append(nums[idx])
+            for sub_tmp in tmp:
+                sub_list.append(sub_tmp)
+            if not sub_list in ret_list:
+                ret_list.append(sub_list)
+            
+    return ret_list
+    
+    
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        return permutation(nums)  
