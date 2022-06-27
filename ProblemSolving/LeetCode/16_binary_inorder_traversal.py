52ae2d253295e07a2281040b9a3d8606e7b12832
Minjune Kim <infinite.minjune.kim@gmail.com>
2022-06-27 19:21:36 +1000

    Create 16_binary_inorder_traversal.py

---
+++ ProblemSolving/LeetCode/16_binary_inorder_traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        return self._traversal(root)
+    
+    def _traversal(self, curr_node, curr_list = []):
+        if curr_node is None:
+            return []
+                   
+        if curr_node.left is not None:
+            self._traversal(curr_node.left)
+        ret_list = self.visit(curr_node, curr_list)
+        if curr_node.right is not None:
+            self._traversal(curr_node.right)
+            
+        return ret_list
+        
+    def visit(self, curr_node, curr_list = []):
+        mylist = curr_list
+        if curr_node is not None:
+            mylist.append(curr_node.val)
+        
+        return mylist
