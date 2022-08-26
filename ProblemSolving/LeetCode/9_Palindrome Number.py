6479f407c350f6abe14c6f0f85cd355a97357898
Minjune Kim <infinite.minjune.kim@gmail.com>
2022-08-26 14:59:52 +1000

    Update 9_Palindrome Number.py

--- ProblemSolving/LeetCode/9_Palindrome Number.py
+++ ProblemSolving/LeetCode/9_Palindrome Number.py
@@ -10,3 +10,17 @@ class Solution:
                 return False
             
         return True
+
+    
+#n=int(input("Enter number:"))
+#temp=n
+#rev=0
+# reverse a number e.g., 129 -> 921
+#while(n>0):
+#    dig=n%10
+#    rev=rev*10+dig
+#    n=n//10
+#if(temp==rev):
+#    print("The number is a palindrome!")
+#else:
+#    print("The number isn't a palindrome!")
