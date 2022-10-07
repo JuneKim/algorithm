6f8f913c50de06d480fa8ab95a9a76bff33705ce
Minjune Kim <infinite.minjune.kim@gmail.com>
2022-10-08 09:37:40 +1000

    Create 191_Number of 1 Bits.py

---
+++ ProblemSolving/LeetCode/191_Number of 1 Bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        cnt = 0
+        tmp_cnt = n
+        while tmp_cnt != 0:
+            if tmp_cnt & 1 == 1:
+                cnt += 1
+            tmp_cnt = tmp_cnt >> 1
+                
+        return cnt
