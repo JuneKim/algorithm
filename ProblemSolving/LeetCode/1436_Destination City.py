e2e20bfc43cfd7013e362d74cf4a91d02ee497dd
MJ.KIM <infinite.minjune.kim@gmail.com>
2023-03-17 14:21:46 +1000

    Update 1436_Destination City.py

--- ProblemSolving/LeetCode/1436_Destination City.py
+++ ProblemSolving/LeetCode/1436_Destination City.py
@@ -17,6 +17,18 @@ class Solution:
         #print (li_dests)
         return li_dests[0]
     
+## runtime: 95%, memory: 30%
+class Solution1:
+    def destCity(self, paths: List[List[str]]) -> str:    
+        lst=[]
+        arr=[]
+        for i in paths:
+            lst.append(i[0])
+            arr.append(i[1])
+        ptr=set(lst)
+        ptr2=set(arr)
+        return list(ptr2-ptr)[0]
+    
 data = [[["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]], [["B","C"],["D","B"],["C","A"]], [["A","Z"]]]
 results = ["Sao Paulo" , "A", "Z"]
 is_success = True
