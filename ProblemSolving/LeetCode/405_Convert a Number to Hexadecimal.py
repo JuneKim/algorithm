d83e2f58d1e2f15a84ba7428ed6468c14256ac9d
MJ.KIM <infinite.minjune.kim@gmail.com>
2023-03-21 10:48:19 +1000

    Create 405_Convert a Number to Hexadecimal.py

---
+++ ProblemSolving/LeetCode/405_Convert a Number to Hexadecimal.py
@@ -0,0 +1,39 @@
+## runtime: 17.78%, memory: 49.32%
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        if num > 0:
+            remainder = num
+        else:
+            remainder = 4294967296 + num
+            
+        mymap = {10:"a", 11:"b", 12:"c", 13:"d", 14:"e", 15:"f"}
+        nums = []
+        ## negative??
+        while remainder > 0:
+            cal = remainder % 16
+            if cal > 9:
+                nums.append(mymap[cal])
+            else:
+                nums.append(str(cal))
+            remainder = remainder // 16
+
+        return "".join([n for n in nums[::-1]])
+
+data = [26, -1]
+results = ["1a", "ffffffff"]
+is_success = True
+    
+sol = Solution()
+for idx, _data in enumerate(data):
+    ret = sol.toHex(_data)
+    if ret != results[idx]:
+        is_success = False
+        
+        print (f"Failed: idx:{idx}")
+        print (f"Outputs: {ret}")
+        print (f"Expected: {results[idx]}")
+        
+if is_success:
+    print ("Success!!")
